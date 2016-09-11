8ebf8e781a24d70bff7ae9074e73b5449bdf278a
FengQiang <498460828@qq.com>
2016-09-11 16:40:49 +0800

    添加测试demo

---
+++ FQAlertViewDemo/FQAlertViewDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FQAlertViewDemo
+//
+//  Created by 奉强 on 16/9/11.
+//  Copyright © 2016年 奉强. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
