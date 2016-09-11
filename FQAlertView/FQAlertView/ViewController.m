9a2ef7e478db8088cbe3afcb7067d7b4362ae30e
FengQiang <498460828@qq.com>
2016-09-11 16:38:12 +0800

    初始化工程

---
+++ FQAlertView/FQAlertView/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  FQAlertView
+//
+//  Created by 奉强 on 16/9/11.
+//  Copyright © 2016年 奉强. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ FQAlertView/FQAlertView/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  FQAlertView
+//
+//  Created by 奉强 on 16/9/11.
+//  Copyright © 2016年 奉强. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
